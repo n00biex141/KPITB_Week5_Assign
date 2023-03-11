f7ab8fde8b643fef7040d679852a96084d157da3
n00biex141 <n00biex141@gmail.com>
2023-03-11 12:35:52 +0500

    firebase intregation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:week5_assignment/firebase_options.dart';
 
 import 'package:week5_assignment/UI/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
